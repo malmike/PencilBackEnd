aec864b1f21b43e84b852e6cec63f1f651824426
Michael Male <malmike21@gmail.com>
2021-02-01 15:50:05 +0300

    change from using dotenv to nconf

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,17 @@
 import express from "express";
-import dotenv from "dotenv";
+import nconf from "nconf";
 import { MongoDBConnection } from "./db_connection";
 import questionRoutes from './routes/question';
 import cors from "cors";
 import healthCheckRoute from "./routes/health-check";
 
-// initialize configuration
-dotenv.config();
+// Read in keys and secrets. Using nconf use can set secrets via
+// environment variables, command-line arguments, or a keys.json file.
+nconf.argv().env().file('keys.json');
 
 const app = express();
-const port = process.env.SERVER_PORT || 8080; // default port to listen
-const mongoURI = process.env.MONGO_URI || "";
+const port = nconf.get('port') || 8080; // default port to listen
+const mongoURI = nconf.get('mongoURI');
 
 // setup database connection
 const mongodb = new MongoDBConnection(mongoURI)
